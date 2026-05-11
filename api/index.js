4dc130ed93bf45e97c753e2f4b923981289ca287
SUNIL-RAJPUT-495 <sunilsinghshekhawat495@gmail.com>
2026-05-11 18:26:14 +0530

    updated index

--- api/index.js
+++ api/index.js
@@ -14,12 +14,22 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-    "http://localhost:5173", 
-    "https://solynora-solar-solution.vercel.app",
-    "https://solynora-solar-solution-jidi.vercel.app"
-  ],
-  credentials: true
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173", 
+      "https://solynora-solar-solution.vercel.app",
+      "https://solynora-solar-solution-jidi.vercel.app"
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.endsWith(".vercel.app")) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  optionsSuccessStatus: 200
 }));
 app.use(morgan('dev'));
 
